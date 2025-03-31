45ef56b400fce52935b6513dcecacc9b267728c5
shibasishdas043 <shibasishdas043@gmail.com>
2025-03-31 12:49:09 +0530

    ok

--- STORE LOCATOR (LEAFLET) MINI PROJECT/app.js
+++ STORE LOCATOR (LEAFLET) MINI PROJECT/app.js
@@ -12,6 +12,10 @@ function renderList(){
        const a = document.createElement("a");
        const p = document.createElement("p");
 
+       a.addEventListener("click", () => {
+           flyToStore(iterator);
+       })
+
        div.classList.add("shop-name");
        a.innerText = iterator.properties.name;
        a.href = "#";
@@ -26,15 +30,52 @@ function renderList(){
 renderList();
 
 
-function onEachFeature(){
-
+function makePopupContent(iterator){
+    return `
+        <div>
+            <h4>${iterator.properties.name}</h4>
+            <p>${iterator.properties.address}</p>
+            <div class="phone-number">
+                <a href="telephone:${iterator.properties.phone}">${iterator.properties.phone}</a>
+            </div>
+        </div>
+    `;
 }
+
+function onEachFeature(feature, layer){
+    layer.bindPopup(makePopupContent(feature), {closeButton: false, offset: L.point(0, -8)});
+}
+
+
+// const customIcon = L.icon({
+//     iconUrl: "marker.png",
+//     iconSIze: [20, 10],
+// });
 
 
 const shopsLayer = L.geoJSON(storeList, {
     onEachFeature: onEachFeature,
     pointToLayer: function (feature, latlng) {
-        return L.marker(latlng);
+        return L.marker(latlng); // DEFAULT LEAFLET ICONS
+        // return L.marker(latlng, {icon:customIcon}); // CUSTOM ICONS
     }
 });
 shopsLayer.addTo(renderMap);
+
+
+function flyToStore(iterator){
+    const lat = iterator.geometry.coordinates[1];
+    const lng = iterator.geometry.coordinates[0]
+
+    renderMap.flyTo([lat, lng], 18, {
+        duration: 10
+    });
+    L.popup({closeButton: false, offset: L.point(0, -8)})
+        .setLatLng([lat, lng])
+        .setContent(makePopupContent(iterator))
+        .openOn(renderMap);
+
+
+}
+
+
