a0d4f91c6bfbc489bed2faadf25f1149c7f5028a
shibasishdas043 <shibasishdas043@gmail.com>
2025-03-31 11:05:02 +0530

    ok

--- STORE LOCATOR (LEAFLET) MINI PROJECT/app.js
+++ STORE LOCATOR (LEAFLET) MINI PROJECT/app.js
@@ -1,6 +1,26 @@
 const renderMap = L.map("map").setView([22.9074872, 79.07306671], 5);
 const tileUrl = "https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}@2x.png?key=ttb3xhwMPjIny8cZI1hO";
 
-const author = "Shibasish Das"
-const baseTileLayer = L.tileLayer(tileUrl, {"Made By": author});
+const baseTileLayer = L.tileLayer(tileUrl, {});
 baseTileLayer.addTo(renderMap);
+
+function renderList(){
+    const ul = document.querySelector(".list");
+    storeList.forEach((iterator) => {
+       const li = document.createElement("li");
+       const div = document.createElement("div");
+       const a = document.createElement("a");
+       const p = document.createElement("p");
+
+       div.classList.add("shop-name");
+       a.innerText = iterator.properties.name;
+       a.href = "#";
+       p.innerText = iterator.properties.address;
+
+       div.appendChild(a);
+       div.appendChild(p);
+       li.appendChild(div);
+       ul.appendChild(li);
+    });
+}
+renderList();
