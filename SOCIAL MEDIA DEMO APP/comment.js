10c6d5a68a05e483ff4fc3561f8afbc7e21542b9
shibasishdas043 <shibasishdas043@gmail.com>
2025-05-01 23:06:22 +0530

    fixed some CSS And HTML

--- SOCIAL MEDIA DEMO APP/comment.js
+++ SOCIAL MEDIA DEMO APP/comment.js
@@ -9,11 +9,12 @@ const commentInput = document.querySelector("#comment-input");
 const commentSendButton = document.querySelector("#comment-send-btn");
 
 commentButton.addEventListener("click", () => {
-  if (commentSectionForm.classList.contains("hidden")) {
-    commentSectionForm.classList.remove("hidden");
-  } else {
-    commentSectionForm.classList.add("hidden");
-  }
+  // if (commentSectionForm.classList.contains("hidden")) {
+  //   commentSectionForm.classList.remove("hidden");
+  // } else {
+  //   commentSectionForm.classList.add("hidden");
+  // }
+  commentSectionForm.classList.toggle("hidden");
 });
 
 // const renderComments = document.createElement("div");
@@ -22,18 +23,26 @@ commentButton.addEventListener("click", () => {
 // const extractedComment = document.createElement("span");
 // extractedComment.classList.add("comments");
 
-
 // const commentRemoveButton = document.createElement("button");
 // commentRemoveButton.id = "remove-comment";
 
 // console.log(renderComments);
 
-commentSendButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  
-  const comments = commentInput.value.trim();
-  if (!comments) return;
+// const renderComments = document.createElement("div");
+// renderComments.classList.add("comments-render");
+//
+// const extractedComment = document.createElement("span");
+// extractedComment.classList.add("comments");
+//
+// const commentRemoveButton = document.createElement("button");
+// commentRemoveButton.id = "remove-comment";
+//
+// const removeCommentImage = document.createElement("img");
+// removeCommentImage.src = "./icons_img/removeBtn.svg";
+// removeCommentImage.alt = "Remove-Comment-Img";
+// removeCommentImage.id = "remove-comment-img";
 
+function add_new_comment(comments) {
   const renderComments = document.createElement("div");
   renderComments.classList.add("comments-render");
 
@@ -45,15 +54,29 @@ commentSendButton.addEventListener("click", (event) => {
 
   const removeCommentImage = document.createElement("img");
   removeCommentImage.src = "./icons_img/removeBtn.svg";
-  removeCommentImage.alt = "Remove-Comment-Img"
+  removeCommentImage.alt = "Remove-Comment-Img";
   removeCommentImage.id = "remove-comment-img";
 
   extractedComment.innerText = comments;
-
 
   renderComments.appendChild(extractedComment);
   renderComments.appendChild(commentRemoveButton);
   commentRemoveButton.appendChild(removeCommentImage);
   socialMedia.appendChild(renderComments);
+}
+
+commentSendButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const comments = commentInput.value.trim();
+
+  if (!comments) {
+    commentSectionForm.classList.add("hidden");
+    return;
+  }
+  add_new_comment(comments);
+
   commentSectionForm.classList.add("hidden");
+  
+  commentInput.value = "";
 });
