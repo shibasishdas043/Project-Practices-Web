218efeb10d09cf8988ec6d0f783d310aabeb92b6
Shibasish Das <107395100+shibasishdas043@users.noreply.github.com>
2026-02-11 10:30:25 +0530

    ok

--- SIMPLE TELEGRAM BOT/index.js
+++ SIMPLE TELEGRAM BOT/index.js
@@ -1,5 +1,37 @@
 const TelegarmBOT = require("node-telegram-bot-api");
+const axios = require("axios");
 
-const Token = "8442296282:AAG58KLrjjcbh-DlsgXgK4sa_URP8OoJ3ug";
+const dotenv = require("dotenv");
+dotenv.config();
 
-console.log(process.env.Token);
+const bot = new TelegarmBOT(process.env.TELEGRAM_TOKEN, { polling: true });
+
+bot.onText("joke", async (msg) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  // const chatId = msg.chat.id;
+  //   const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  //   bot.sendMessage(chatId, resp);
+
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  //   bot.sendMessage(chatId, "Hello");
+
+  const response = await axios.get(
+    "http://www.official-joke-api.appspot.com//random_joke",
+    );
+    const setup = response.data.setup;
+    const punchline = response.data.punchline;
+
+  if (response !== null) {
+    bot.sendMessage(chatId, `${setup}, ${punchline}`);
+  }
+  console.log(`Recieved`, msg);
+});
+
+console.log(process.env.TELEGRAM_TOKEN);
